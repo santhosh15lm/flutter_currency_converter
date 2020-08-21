903a225702e86bafdf84f4955c168aa88b9c9fc7
Santhosh M <55490501+santhosh15lm@users.noreply.github.com>
2020-08-22 00:09:45 +0530

    Update showcurrency.dart

--- lib/showcurrency.dart
+++ lib/showcurrency.dart
@@ -6,7 +6,6 @@ import 'dart:io';
 
 class showcurrency extends StatefulWidget {
   showcurrency({Key key, this.number}) : super(key: key);
-  //List<namemodel> namewithprice = [];
   String number;
   @override
   _showcurrencyState createState() => _showcurrencyState(number);
@@ -15,40 +14,7 @@ class showcurrency extends StatefulWidget {
 class _showcurrencyState extends State<showcurrency> {
   String num;
   _showcurrencyState(this.num);
-  //static const url = 'https://api.exchangeratesapi.io/latest?base=INR';
-
-  // void initState() {
-  //   final response = getcurrencyval();
-  //   Map map = json.decode(response.body);
-  //   Map rates = map['rates'];
-  //   //Iterable rates = map['rates'];
-  //   //namewithprice = rates.map((model) => namemodel.fromJson(model)).toList();
-  //   rates.forEach((key, value) {
-  //     name.add(key);
-  //     price.add(value);
-  //   });
-  // }
-
-  // List<String> name = [];
-  // List<double> price = [];
-  // bool isLoading;
-  // setLoading(bool state) {
-  //   setState(() => isLoading = state);
-  // }
-
-  // getcurrencyval() async {
-  //   try {
-  //     setLoading(true);
-  //     await getdata();
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // }
-
-  // getdata() async {
-  //   final response = await http.get(url);
-  //   return response;
-  // }
+  
   Map _curconvert = new Map();
   Map _rates = new Map();
   void _getData() async {
